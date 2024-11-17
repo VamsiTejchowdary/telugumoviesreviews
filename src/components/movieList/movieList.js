cf6f2809403cf0324edad95bd2003739e5f5a981
D.VAMSI TEJ CHOWDARY <d.vamsitej333@gmail.com>
2024-11-16 19:30:20 -0500

    updated with missing depdencies

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "./movieList.css";
 import { useParams } from "react-router-dom";
 import Cards from "../card/card";
@@ -7,26 +7,26 @@ const MovieList = () => {
     const [movieList, setMovieList] = useState([]);
     const { type } = useParams(); // Get 'type' from the URL
 
-    useEffect(() => {
-        getData();
-    }, [type]); // Re-fetch when 'type' changes
-
-    const getData = () => {
+    // Memoizing the getData function using useCallback to prevent it from being recreated on every render
+    const getData = useCallback(() => {
         let apiUrl = "";
 
         if (type === "popular") {
             apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=acaba87d72eba033de2058214994a722&with_original_language=te&primary_release_date.gte=2015-01-01&sort_by=popularity.desc`;
         } else if (type === "upcoming") {
             apiUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=acaba87d72eba033de2058214994a722&language=te&region=IN&release_date.gte=${new Date().toISOString().split("T")[0]}`;
-        }
-        else{
+        } else {
             apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=acaba87d72eba033de2058214994a722&with_original_language=te&primary_release_date.gte=2022-01-01&primary_release_date.lte=2024-12-31&sort_by=popularity.desc`;
         }
 
         fetch(apiUrl)
             .then((res) => res.json())
             .then((data) => setMovieList(data.results));
-    };
+    }, [type]); // Include 'type' as a dependency since it changes based on the URL
+
+    useEffect(() => {
+        getData(); // Call getData inside useEffect
+    }, [getData]); // Dependency on the memoized getData function
 
     return (
         <div className="movie__list">
